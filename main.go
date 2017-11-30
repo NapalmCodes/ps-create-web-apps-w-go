a95c57c75194abcff87b3a3c7e8e555fd673efbc
Shawn Vause <svause@zoll.com>
2017-11-30 13:40:18 -0500

    Without handler using built in file server

--- main.go
+++ main.go
@@ -5,8 +5,5 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "public"+r.URL.Path)
-	})
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
 }
