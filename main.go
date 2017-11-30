b5d66ce3e7b23485cf112fd0ef2dbd3738ed4228
Shawn Vause <svause@zoll.com>
2017-11-30 14:19:41 -0500

    Wired up templates

--- main.go
+++ main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
-	"os"
+	"log"
+	"net/http"
 )
 
 func main() {
-	templateString := `Lemonade Stand Supply`
-	t, err := template.New("title").Parse(templateString)
-	if err != nil {
-		fmt.Println(err)
-	}
-	err = t.Execute(os.Stdout, nil)
-	if err != nil {
-		fmt.Println(err)
-	}
+	templates := populateTemplates()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestedFile := r.URL.Path[1:]                //Remove slash leading path
+		t := templates.Lookup(requestedFile + ".html") //So Urls dont have to end in html all the time
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+	http.Handle("/img/", http.FileServer(http.Dir("public")))
+	http.Handle("/css/", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":8000", nil)
+}
+
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+	const basePath = "templates"
+
+	//Every file in base path with html will be parsed
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+
+	return result
 }
